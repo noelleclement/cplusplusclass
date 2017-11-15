23dec15ff8af2049071c61c012e48d85607e885e
noelleclement <noelleclement@hotmail.com>
2017-11-15 10:30:20 +0100

    information program added

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,10 @@
 #include <iostream> 		//header
 using namespace std;		//standard namespace
 
+/*
+This is an example comment for my first C++ program
+*/
+
 int main(){
 	cout << "Hello World";
 
---
+++ PrintInformation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>		//include iostream (duh)
+using namespace std;	//standard namespace
+
+int main(){
+
+	cout << "Hoi ik ben: " << endl << "Noëlle " << endl << "Clement " << endl << "0935050 " << endl << "Rotterdam " << endl;
+
+
+}
+
